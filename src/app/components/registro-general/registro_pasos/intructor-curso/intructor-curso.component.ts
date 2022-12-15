7372a3411e705e36ff0b300864c758ce4ec8b960
patocastill0 <patolzincastillo@gmail.com>
2022-12-14 23:39:40 -0600

    Se crea la vista para agregar un nuevo curso y crear un instructor

---
+++ src/app/components/registro-general/registro_pasos/intructor-curso/intructor-curso.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-intructor-curso',
+  templateUrl: './intructor-curso.component.html',
+  styleUrls: ['./intructor-curso.component.css']
+})
+export class IntructorCursoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
